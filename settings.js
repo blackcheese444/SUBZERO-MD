b294086c71b8a564ef4a0e8a2e0abfc625116802
blackcheese444 <ncubenyasha412@gmail.com>
2025-09-26 08:55:15 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
+  SESSION_ID: "SUBZERO~f2a7042798113452924ba59ec33069e60d61ccb9", // Put Session Id Here Inside "*"
+  OWNER_NUMBER: "263 78378 3284", // Put Owner Number Here 
   PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
   CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
 };
